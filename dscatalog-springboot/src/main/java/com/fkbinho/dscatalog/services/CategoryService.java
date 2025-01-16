f09a85fb85581287681d0670adf6d805d272d288
fkbinho <sir_kleber@hotmail.com>
2025-01-15 22:02:47 -0300

    add findById

--- dscatalog-springboot/src/main/java/com/fkbinho/dscatalog/services/CategoryService.java
+++ dscatalog-springboot/src/main/java/com/fkbinho/dscatalog/services/CategoryService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -19,5 +20,11 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).toList();
+    }
+
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity =obj.get();
+        return new CategoryDTO(entity);
     }
 }

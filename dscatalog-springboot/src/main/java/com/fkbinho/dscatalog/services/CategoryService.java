9cefa7c9e0ee8ad846631bbd1e223de756379def
fkbinho <sir_kleber@homail.com>
2025-01-30 10:30:42 -0300

    Delete category

--- dscatalog-springboot/src/main/java/com/fkbinho/dscatalog/services/CategoryService.java
+++ dscatalog-springboot/src/main/java/com/fkbinho/dscatalog/services/CategoryService.java
@@ -3,10 +3,13 @@ package com.fkbinho.dscatalog.services;
 import com.fkbinho.dscatalog.dto.CategoryDTO;
 import com.fkbinho.dscatalog.entities.Category;
 import com.fkbinho.dscatalog.repositories.CategoryRepository;
+import com.fkbinho.dscatalog.services.exceptions.DatabaseException;
 import com.fkbinho.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -47,6 +50,19 @@ public class CategoryService {
             return new CategoryDTO(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("ID not found: " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("ID not found " + id);
+        }
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
         }
     }
 }

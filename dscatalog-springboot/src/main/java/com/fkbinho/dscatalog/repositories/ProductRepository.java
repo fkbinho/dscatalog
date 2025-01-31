199fa4a3491bcb750d93497f0e27354c8d515ebc
fkbinho <sir_kleber@homail.com>
2025-01-31 11:52:46 -0300

    Product repository, service, resource, GET ok

---
+++ dscatalog-springboot/src/main/java/com/fkbinho/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.fkbinho.dscatalog.repositories;
+
+import com.fkbinho.dscatalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}

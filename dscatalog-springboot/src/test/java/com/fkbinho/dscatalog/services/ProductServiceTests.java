bb7575f8ba17305b8380eed6b2b849a05d745f24
fkbinho <sir_kleber@homail.com>
2025-04-19 10:42:52 -0300

    add behaviors with mockito

--- dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/services/ProductServiceTests.java
+++ dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/services/ProductServiceTests.java
@@ -1,17 +1,25 @@
 package com.fkbinho.dscatalog.services;
 
+import com.fkbinho.dscatalog.entities.Product;
 import com.fkbinho.dscatalog.repositories.ProductRepository;
 import com.fkbinho.dscatalog.services.exceptions.DatabaseException;
 import com.fkbinho.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.fkbinho.dscatalog.tests.Factory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith({SpringExtension.class})
 public class ProductServiceTests {
@@ -25,25 +33,49 @@ public class ProductServiceTests {
     private long existingId;
     private long nonExistingId;
     private long dependentId;
+    private PageImpl<Product> page;
+    private Product product;
 
     @BeforeEach
     void setUp() {
         existingId = 1L;
         nonExistingId = 2L;
         dependentId = 3L;
+        product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
 
-        // Mock the behavior of the repository to do nothing when deleteById is called
+        // Mock the behavior of the repository to return a list of products
+        // when findAll is called with any Pageable
+        Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+
+        // Mock the behavior of the repository to return a product
+        // when save is called
+        Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+
+        // Mock the behavior of the repository to return a product
+        // when findById is called with an existing ID
+        Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+
+        // Mock the behavior of the repository to return an empty Optional
+        // when findById is called with a non-existing ID
+        Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
+
+        // Mock the behavior of the repository to do nothing
+        // when deleteById is called
         Mockito.doNothing().when(repository).deleteById(existingId);
 
         // Mock the behavior of the repository to throw DataIntegrityViolationException
         // when deleteById is called with a dependent ID
         Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 
-        // Mock the behavior of the repository to return true when checking for an existing ID
+        // Mock the behavior of the repository to return true
+        // when checking for an existing ID
         Mockito.when(repository.existsById(existingId)).thenReturn(true);
-        // Mock the behavior of the repository to return false when checking for a non-existing ID
+        // Mock the behavior of the repository to return false
+        // when checking for a non-existing ID
         Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
-        // Mock the behavior of the repository to return true when checking for a dependent ID
+        // Mock the behavior of the repository to return true
+        // when checking for a dependent ID
         Mockito.when(repository.existsById(dependentId)).thenReturn(true);
 
     }

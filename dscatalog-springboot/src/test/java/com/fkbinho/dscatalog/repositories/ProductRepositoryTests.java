788f477f0aed9b1b7d48003e87210b1b05edf70b
fkbinho <sir_kleber@homail.com>
2025-04-19 09:31:02 -0300

    add save and delete tests in ProductRepositoryTests

---
+++ dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,49 @@
+package com.fkbinho.dscatalog.repositories;
+
+import com.fkbinho.dscatalog.entities.Product;
+import com.fkbinho.dscatalog.tests.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
+
+    @Test
+    public void deleteShouldDeleteObjWhenIdExits() {
+        repository.deleteById(1L);
+
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+}

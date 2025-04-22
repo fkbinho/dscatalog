25020c780a732c70e42a0af8043f36d8114de78e
fkbinho <sir_kleber@homail.com>
2025-04-22 20:31:29 -0300

    create ProductServiceIT

---
+++ dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,28 @@
+package com.fkbinho.dscatalog.services;
+
+
+import com.fkbinho.dscatalog.repositories.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+    @Autowired
+    private ProductService service;
+
+    @Autowired
+    private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+}

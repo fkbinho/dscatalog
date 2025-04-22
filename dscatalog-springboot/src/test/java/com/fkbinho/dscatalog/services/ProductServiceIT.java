9f62761a340c6d8f555842d66cde1d9b3be7dc25
fkbinho <sir_kleber@homail.com>
2025-04-22 20:45:48 -0300

    add findAllPaged tests in ProductServiceIT

--- dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/services/ProductServiceIT.java
+++ dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/services/ProductServiceIT.java
@@ -1,6 +1,7 @@
 package com.fkbinho.dscatalog.services;
 
 
+import com.fkbinho.dscatalog.dto.ProductDTO;
 import com.fkbinho.dscatalog.repositories.ProductRepository;
 import com.fkbinho.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -8,11 +9,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
     @Autowired
@@ -30,6 +35,55 @@ public class ProductServiceIT {
         existingId = 1L;
         nonExistingId = 1000L;
         countTotalProducts = 25L;
+    }
+
+    @Test
+    public void findAllPagedShouldReturnPageWhenPage0Size10() {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        // Test to verify that the service returns a page of products
+        // when the page is 0 and size is 10
+        Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+        // Verify that the result is not empty
+        Assertions.assertFalse(result.isEmpty());
+
+        // Verify that the number of page is 0
+        Assertions.assertEquals(0, result.getNumber());
+
+        // Verify that the size of the page is 10
+        Assertions.assertEquals(10, result.getSize());
+
+        // Verify that the total number of products is as expected
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExists() throws Exception {
+        PageRequest pageRequest = PageRequest.of(50, 10);
+
+        // Test to verify that the service returns an empty page
+        // when the page does not exist
+        Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+        // Verify that the result is empty
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnSortedPageWhenSortByName() throws Exception {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+
+        // Test to verify that the service returns a sorted page of products
+        Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+        // Verify that the result is not empty
+        Assertions.assertFalse(result.isEmpty());
+
+        // Verify that the first/second/third product in the page is the expected one
+        Assertions.assertEquals("Macbook Pro", result.getContent().getFirst().getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
     }
 
     @Test

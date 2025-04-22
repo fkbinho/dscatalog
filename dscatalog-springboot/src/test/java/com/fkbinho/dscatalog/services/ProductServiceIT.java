a21ff070527d0ddb5a2608b56cd9f90a7a12c872
fkbinho <sir_kleber@homail.com>
2025-04-22 20:33:20 -0300

    add delete tests in ProductServiceIT

--- dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/services/ProductServiceIT.java
+++ dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/services/ProductServiceIT.java
@@ -2,9 +2,15 @@ package com.fkbinho.dscatalog.services;
 
 
 import com.fkbinho.dscatalog.repositories.ProductRepository;
+import com.fkbinho.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ProductServiceIT {
@@ -24,5 +30,23 @@ public class ProductServiceIT {
         existingId = 1L;
         nonExistingId = 1000L;
         countTotalProducts = 25L;
+    }
+
+    @Test
+    public void deleteShouldDeleteResourceWhenIdExists() throws Exception {
+        service.delete(existingId);
+
+        // Verify that the product was deleted
+        assertEquals(countTotalProducts - 1, repository.count());
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() {
+
+        // Attempt to delete a product with a non-existing ID
+        Assertions.assertThrows(
+                ResourceNotFoundException.class,
+                () -> service.delete(nonExistingId)
+        );
     }
 }

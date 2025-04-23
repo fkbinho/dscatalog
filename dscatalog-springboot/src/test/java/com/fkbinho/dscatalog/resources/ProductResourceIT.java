c5aed6cb50df4312efa95cb9a873be995c22c7ef
fkbinho <sir_kleber@homail.com>
2025-04-22 21:12:33 -0300

    create ProductResourceIT

---
+++ dscatalog-springboot/src/test/java/com/fkbinho/dscatalog/resources/ProductResourceIT.java
@@ -0,0 +1,28 @@
+package com.fkbinho.dscatalog.resources;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private long existingId;
+    private long nonExistingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+}

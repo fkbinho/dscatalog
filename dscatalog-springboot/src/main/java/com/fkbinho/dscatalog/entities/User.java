3dec0c2beca0bd9806d71360b10c1f136957b48d
fkbinho <sir_kleber@homail.com>
2025-04-25 21:30:43 -0300

    ORM mapping, database seeding

--- dscatalog-springboot/src/main/java/com/fkbinho/dscatalog/entities/User.java
+++ dscatalog-springboot/src/main/java/com/fkbinho/dscatalog/entities/User.java
@@ -1,16 +1,27 @@
 package com.fkbinho.dscatalog.entities;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
+    private String lastName;
     private String email;
     private String password;
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
     public User() {
@@ -37,6 +48,14 @@ public class User {
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
